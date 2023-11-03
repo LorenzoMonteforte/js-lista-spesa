e933c6a7d3226c262dbd72c4c4cfc8c18b213838
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-03 14:51:01 +0100

    Bonus

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Array lista della spesa
 const listaSpesa = ["Olio", "Pasta", "Latte", "Burro", "Riso", "Panna", "Uova", "Aceto", "Pangrattato", "Limoni"];
 // Stampa in pagina e in console la lista della spesa
+const lista = document.getElementById("lista");
 let i = 0;
 while(i<listaSpesa.length){
-    const div = document.createElement("div");
-    div.textContent = listaSpesa[i];
-    document.body.appendChild(div);
+    const li = document.createElement("li");
+    li.textContent = listaSpesa[i];
+    lista.appendChild(li);
     console.log(listaSpesa[i]);
     i++;
 }
